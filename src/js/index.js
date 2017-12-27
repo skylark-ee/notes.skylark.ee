76bf70a8e703d7df5ab3711a869512012becd699
Flaki <git@flaki.hu>
2017-12-27 22:08:36 +0100

    fix detect current host on frontend

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,10 @@
 const EDITOR = document.querySelector('textarea')
+const HOST = window.location.host
 
 autosize(EDITOR)
 
-//fetch(`http://notes.skylark.local/api/notes.json`)
-fetch(`http://notes.skylark.local/api/notes/skylark-processes.md`).then(n => n.text()).then(note => {
+//fetch(`http://${HOST}/api/notes.json`)
+fetch(`http://${HOST}/api/notes/skylark-processes.md`).then(n => n.text()).then(note => {
   EDITOR.value = note
   autosize.update(EDITOR)
 })
