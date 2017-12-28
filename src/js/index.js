2bb097f5ae1bc0588396e98c567a3fb800c99852
Flaki <git@flaki.hu>
2017-12-28 10:25:03 +0100

    add document switching

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,16 @@ const HOST = window.location.host
 
 autosize(EDITOR)
 
-//fetch(`http://${HOST}/api/notes.json`)
-fetch(`http://${HOST}/api/notes/skylark-processes.md`).then(n => n.text()).then(note => {
-  EDITOR.value = note
-  autosize.update(EDITOR)
+fetch(`http://${HOST}/api/docs.json`).then(r => r.json()).then(docs => {
+  $('aside select').innerHTML = docs.map(doc => `<option>${doc}</option>`).join('')
 })
+
+function load(doc) {
+  return fetch(`http://${HOST}/api/notes/${doc}`).then(n => n.text()).then(note => {
+    EDITOR.value = note
+    autosize.update(EDITOR)
+  })
+}
 
 
 $('aside ul :nth-child(1)>button').addEventListener('click', event => $('aside').classList.toggle('closed'))
@@ -23,4 +28,8 @@ $('aside ul :nth-child(5)>button').addEventListener('click', event => {
   let s = parseInt(EDITOR.style.fontSize)
   EDITOR.style.fontSize = (isNaN(s) ? 90 : s-10)+'%'
   autosize.update(EDITOR)
+})
+
+$('aside select').addEventListener('change', event => {
+  load(event.target.value)
 })
